e7c778f9d8fa32761ab79ea50459e3b77a0ec769
StrinGhost <153989055+StrinGhost@users.noreply.github.com>
2024-10-27 09:41:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,18 +6,14 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-if [ ! -d "/root" ]; then
-  echo "/root directory does not exist"
-  exit 1
-fi
-
+CMD_OUTPUT="$CMD_OUTPUT"
 #######################################################################
 
 
 # Update package database and install packages for Debian-based systems
-sudo apt update -y &>>installation_output.txt & apt install lolcat figlet -y &>>installation_output.txt 
+sudo apt update -y $CMD_OUTPUT & sudo apt install lolcat figlet -y $CMD_OUTPUT 
 clear
-echo """
+echo "
 ⠀⠀⠀⠀⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡔⣖⡶⠀⠀⡆⡴⠒⠒⢦⣄⠀⠲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⡔⢂⡄⠀⠀⠀⠀⠀⠀⠀⢰⢍⢸⣜⠀⠀⢸⢀⠃⠀⠀⠀⠙⢦⣀⣹⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠈⠩⠊⠁⠀⠀⠀⠀⠀⠀⣼⠎⡆⣿⠀⠀⢸⢸⠀⠀⠀⠀⠀⠈⢉⡿⠋⠉⢢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -54,67 +50,64 @@ echo """
 ⠈⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀
 ⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀
 ⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡉⠉⠋⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠙⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠇⠀⠰⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿""" | lolcat
-echo ""
-echo "                                                 " | lolcat
-echo "$(figlet -f slant 'StrinGhost')" | lolcat
-echo "                                                 " | lolcat
-echo "##################################################" | lolcat
-echo "#                                                #" | lolcat
-echo "#  GitHub: https://github.com/StrinGhost         #" | lolcat
-echo "#                                                #" | lolcat
-echo "##################################################" | lolcat
-echo ""
-echo ""
-echo "$(figlet -f slant 'Kali Linux Customization')" | lolcat
-echo ""
-echo ""
+⠀⠀⠀⠀⠀⠙⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠇⠀⠰⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿" | lolcat
+echo "\n\n\n"
+echo "$(figlet -f slant 'KaliGhost')" | lolcat
+echo "\n\n\n"
+echo "##################################################
+#  ____ ____ ____
+# ╭━━━┳━━━╮  StrinGhost
+# ┃╭━╮┃╭━╮┃  https://www.github.com/StrinGhost
+# ┃╰━━┫┃╱╰╯  https://www.reddit.com/u/StrinGhost
+# ╰━━╮┃┃╭━╮  https://x.com/StrinGhost
+# ┃╰━╯┃╰┻━┃  https://discord.gg/wtymSq5c
+# ╰━━━┻━━━╯
+##################################################" | lolcat
+echo "\n\n\n"
+
 echo "Updating package database and installing essential packages..." | lolcat
-sudo apt-get install -y zsh python3 npm arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo papirus-icon-theme neofetch picom betterlockscreen wtmpdb bat eza net-tools fzf tmux open-vm-tools btop stow ripgrep gh  &>>installation_output.txt
-sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson &>>installation_output.txt
-sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev i3lock-fancy polybar &>>installation_output.txt
+sudo apt-get install -y zsh python3 npm arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo papirus-icon-theme neofetch picom betterlockscreen wtmpdb bat eza net-tools fzf tmux open-vm-tools btop stow ripgrep gh  $CMD_OUTPUT
+sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson $CMD_OUTPUT
+sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev i3lock-fancy polybar $CMD_OUTPUT
 
 # install Alacritty , obsidian
-echo "Installing alacritty , obsidian" | lolcat
 if dpkg -l | grep -q "obsidian" || which obsidian > /dev/null; then
    echo "Obsidian already Installed." | lolcat
 else
    echo "Installing Obsidian" | lolcat
-   wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/obsidian_1.6.7_amd64.deb &>>installation_output.txt
-   sudo dpkg -i obsidian_1.6.7_amd64.deb &>>installation_output.txt
-   sudo apt install -f -y &>>installation_output.txt
-   rm alacritty_0.12.0_amd64_bullseye.deb
+   wget -q https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/obsidian_1.6.7_amd64.deb $CMD_OUTPUT
+   sudo dpkg -i obsidian_1.6.7_amd64.deb $CMD_OUTPUT
+   sudo apt install -f -y $CMD_OUTPUT
+   rm obsidian_1.6.7_amd64.deb
 fi
 
 if dpkg -l | grep -q "alacritty" || which alacritty > /dev/null; then
    echo "Alacritty already Installed." | lolcat
 else
    echo "Installing Alacritty" | lolcat
-   wget https://github.com/barnumbirr/alacritty-debian/releases/download/v0.12.0-1/alacritty_0.12.0_amd64_bullseye.deb &>>installation_output.txt
-   sudo dpkg -i alacritty_0.12.0_amd64_bullseye.deb &>>installation_output.txt
-   sudo apt install -f -y &>>installation_output.txt
+   wget -q https://github.com/barnumbirr/alacritty-debian/releases/download/v0.12.0-1/alacritty_0.12.0_amd64_bullseye.deb $CMD_OUTPUT
+   sudo dpkg -i alacritty_0.12.0_amd64_bullseye.deb $CMD_OUTPUT
+   sudo apt install -f -y $CMD_OUTPUT
    rm alacritty_0.12.0_amd64_bullseye.deb
 fi
 
 # Install pywal using pip
 echo "Installing pywal..." | lolcat
-pip3 install pywal &>>installation_output.txt
+pip3 install pywal $CMD_OUTPUT
 
 # make directories 
 echo "Setting up directories and fonts..." | lolcat
-mkdir -p ~/.local/share/fonts/
-mkdir ~/.fonts
 mkdir ~/.config
 mkdir ~/Pictures/screenshots
-mkdir -p ~/Desktop/ovpn
+sudo mkdir -p /usr/share/fonts/nerd-fonts
 
 # Check and install Iosevka Nerd Font
 if fc-list | grep -qi "Iosevka"; then
     echo "Iosevka Nerd Font already installed" | lolcat
 else
     echo "Installing Iosevka fonts" | lolcat
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip &>>installation_output.txt
-    unzip Iosevka.zip -d ~/.local/share/fonts/ &>>installation_output.txt
+    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip $CMD_OUTPUT
+    unzip Iosevka.zip -d /usr/share/fonts/nerd-fonts/Iosevka $CMD_OUTPUT
     rm Iosevka.zip
 fi
 
@@ -123,8 +116,8 @@ if fc-list | grep -qi "RobotoMono"; then
     echo "RobotoMono Nerd Font already installed" | lolcat
 else
     echo "Installing RobotoMono fonts" | lolcat
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip &>>installation_output.txt
-    unzip RobotoMono.zip -d ~/.local/share/fonts/ &>>installation_output.txt
+    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip $CMD_OUTPUT
+    unzip RobotoMono.zip -d /usr/share/fonts/nerd-fonts/RobotoMono $CMD_OUTPUT
     rm RobotoMono.zip
 fi
 
@@ -133,45 +126,43 @@ if fc-list | grep -qi "Hack"; then
     echo "Hack Nerd Font already installed" | lolcat
 else
     echo "Installing Hack Nerd fonts" | lolcat
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip &>>installation_output.txt
-    unzip Hack.zip -d ~/.local/share/fonts/ &>>installation_output.txt
+    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip $CMD_OUTPUT
+    unzip Hack.zip -d /usr/share/fonts/nerd-fonts/Hack $CMD_OUTPUT
     rm Hack.zip
 fi
-fc-cache -f
+sudo fc-cache -f
 
 # Copy configuration files
-echo "Copying configuration files..." | lolcat
+echo "\nCopying configuration files..." | lolcat
 cp -r config/* ~/.config/
 chmod +x chbg
 cp chbg ~/.chbg
 cp -r images ~/.images
-cp -r /usr/bin/batcat /usr/bin/cat
-cp -r /usr/bin/eza /usr/bin/ls
-cp -r clipse /usr/local/bin/
-cp nord.rasi /usr/share/rofi/themes/
-cp -r copy /usr/local/bin/
+sudo cp -r /usr/bin/batcat /usr/bin/cat
+sudo cp -r /usr/bin/eza /usr/bin/ls
+sudo cp nord.rasi /usr/share/rofi/themes/
+sudo cp -r copy /usr/local/bin/
 
 
 # Make scripts executable
-chmod +x ~/.config/polybar/launch.sh
-chmod +x ~/.config/i3/clipboard_fix.sh 
-chmod +x ~/.config/ranger/scope.sh
-chmod +x ~/.config/rofi/power/powermenu.sh
+sudo chmod +x ~/.config/polybar/launch.sh
+sudo chmod +x ~/.config/i3/clipboard_fix.sh 
+sudo chmod +x ~/.config/rofi/power/powermenu.sh
 
 
 # Configure Betterlockscreen 
 echo "Configuring betterlockscreen..." | lolcat
-betterlockscreen -u ~/.images/0.jpeg &>>installation_output.txt
+sudo betterlockscreen -u ~/.images/0.jpeg $CMD_OUTPUT
 
 # Install tpm and Configure tmux
 if [ -d "~/.tmux/plugins/tpm" ]; then
    echo "tpm already installed." | lolcat
 else
    echo "Installing tpm..." | lolcat
-   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &>>installation_output.txt
+   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm $CMD_OUTPUT
    echo "Installing Tmux plugins..." | lolcat 
    if [ -f "~/.tmux/plugins/tpm/scripts/install_plugins.sh" ]; then
-       bash " ~/.tmux/plugins/tpm/scripts/install_plugins.sh"
+       bash " ~/.tmux/plugins/tpm/scripts/install_plugins.sh" $CMD_OUTPUT
    fi
 fi
 
@@ -180,40 +171,43 @@ if [ -d "/opt/nvim" ]; then
    echo "Neovim Already installed" | lolcat
 else
    echo "Installing Neovim" | lolcat
-   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz &>>installation_output.txt
+   wget -q https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz -O nvim-linux64.tar.gz $CMD_OUTPUT
    sudo tar -C /opt -xzf nvim-linux64.tar.gz
-   mv /opt/nvim-linux64 /opt/nvim
+   sudo mv /opt/nvim-linux64 /opt/nvim
+   rm nvim-linux64.tar.gz
    echo "Installing NvChad after installation completes. Run 'nvim' to download plugins then ':MasonInstallAll'." | lolcat
-   git clone https://github.com/NvChad/starter ~/.config/nvim
+   git clone https://github.com/NvChad/starter ~/.config/nvim $CMD_OUTPUT
 fi
 
 # Install Oh My Zsh
 echo "Installing Oh My Zsh..." | lolcat
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --keep-zshrc &>>installation_output.txt
 cp zshrc ~/.zshrc
+sh -c "$(curl -fsSL https://install.ohmyz.sh)" "" --unattended $CMD_OUTPUT
+chsh -s $(which zsh)
+
+# Remove Packages
+sudo apt remove --purge qterminal
+sudo apt autoremove
 
 # Prompt user to update packages
-echo "(Recommended) Do you want to update the packages (y/n):" | lolcat
+echo "\n\n\n(Recommended) Do you want to update the packages (y/n):" | lolcat
 read -r upackage
 
 # Check user input and take action
 if [[ "$upackage" == "n" || "$upackage" == "N" ]]; then
-    echo "Please update your packages after a reboot using the command 'sudo apt update && sudo apt upgrade'" | lolcat
+    echo "\nPlease update your packages after a reboot using the command 'sudo apt update && sudo apt upgrade'" | lolcat
 else
     echo "Updating Packages" | lolcat
-    sudo apt update  -y &>>installation_output.txt
-    sudo apt upgrade -y &>>installation_output.txt 
+    sudo apt update  -y $CMD_OUTPUT
+    echo "\nUpgrading Packages this may take sometime to complete\n" | lolcat
+    sudo apt upgrade -y 
+    clear
 fi
 
 # Prompt user for reboot
-echo "(StrinGhost) (+) Kali Linux Customisation Completed!" | lolcat
-echo ""
-echo ""
-echo "Outputs saved in installation_output.txt in case of any error consider checking it out." | lolcat
-echo ""
-echo ""
+echo "\n\n(StrinGhost) (+) KaliGhost Installation Completed!\n\nOutputs saved in installation_output.txt in case of any error consider checking it out." | lolcat
 
-echo "Reboot required to see the changes. Do you want to reboot now (y/n):" | lolcat
+echo "\n\n\nReboot required to see the changes. Do you want to reboot now (y/n):" | lolcat
 read -r option
 
 # Check user input and take action
