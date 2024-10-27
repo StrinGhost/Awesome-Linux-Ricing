0f65bf860450ae32e844f482714d163fd2223640
StrinGhost <153989055+StrinGhost@users.noreply.github.com>
2024-10-27 12:07:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,11 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-CMD_OUTPUT="&>>kalighost.out"
 #######################################################################
 
 
 # Update package database and install packages for Debian-based systems
-sudo apt update -y $CMD_OUTPUT & sudo apt install lolcat figlet -y $CMD_OUTPUT 
+sudo apt update -y &>> kalighost.out & sudo apt install lolcat figlet -y &>> kalighost.out 
 clear
 echo "
 ⠀⠀⠀⠀⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡔⣖⡶⠀⠀⡆⡴⠒⠒⢦⣄⠀⠲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -66,17 +65,17 @@ echo "##################################################
 echo -e "\n\n\n"
 
 echo "Updating package database and installing essential packages..." | lolcat
-sudo apt-get install -y zsh python3 npm arandr flameshot arc-theme feh i3 polybar lxappearance python3-pip rofi unclutter-xfixes cargo papirus-icon-theme neofetch picom betterlockscreen wtmpdb bat eza net-tools fzf tmux open-vm-tools btop ripgrep gh  $CMD_OUTPUT
-sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev $CMD_OUTPUT
+sudo apt-get install -y zsh python3 npm arandr flameshot arc-theme feh i3 polybar lxappearance python3-pip rofi unclutter-xfixes cargo papirus-icon-theme neofetch picom betterlockscreen wtmpdb bat eza net-tools fzf tmux open-vm-tools btop ripgrep gh  &>> kalighost.out
+sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev &>> kalighost.out
 
 # install Alacritty & Obsidian
 if sudo dpkg -l | grep -q "obsidian" || which obsidian > /dev/null; then
    echo "Obsidian already Installed." | lolcat
 else
    echo "Installing Obsidian" | lolcat
-   wget -q https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/obsidian_1.6.7_amd64.deb $CMD_OUTPUT
-   sudo dpkg -i obsidian_1.6.7_amd64.deb $CMD_OUTPUT
-   sudo apt install -f -y $CMD_OUTPUT
+   wget -q https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/obsidian_1.6.7_amd64.deb &>> kalighost.out
+   sudo dpkg -i obsidian_1.6.7_amd64.deb &>> kalighost.out
+   sudo apt install -f -y &>> kalighost.out
    rm obsidian_1.6.7_amd64.deb
 fi
 
@@ -84,15 +83,15 @@ if sudo dpkg -l | grep -q "alacritty" || which alacritty > /dev/null; then
    echo "Alacritty already Installed." | lolcat
 else
    echo "Installing Alacritty" | lolcat
-   wget -q https://github.com/barnumbirr/alacritty-debian/releases/download/v0.12.0-1/alacritty_0.12.0_amd64_bullseye.deb $CMD_OUTPUT
-   sudo dpkg -i alacritty_0.12.0_amd64_bullseye.deb $CMD_OUTPUT
-   sudo apt install -f -y $CMD_OUTPUT
+   wget -q https://github.com/barnumbirr/alacritty-debian/releases/download/v0.12.0-1/alacritty_0.12.0_amd64_bullseye.deb &>> kalighost.out
+   sudo dpkg -i alacritty_0.12.0_amd64_bullseye.deb &>> kalighost.out
+   sudo apt install -f -y &>> kalighost.out
    rm alacritty_0.12.0_amd64_bullseye.deb
 fi
 
 # Install pywal using pip
 echo "Installing pywal..." | lolcat
-pip3 install pywal $CMD_OUTPUT
+pip3 install pywal &>> kalighost.out
 
 # make directories 
 echo "Setting up directories and fonts..." | lolcat
@@ -105,8 +104,8 @@ if fc-list | grep -qi "Iosevka"; then
     echo "Iosevka Nerd Font already installed" | lolcat
 else
     echo "Installing Iosevka Nerd font" | lolcat
-    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip $CMD_OUTPUT
-    unzip Iosevka.zip -d /usr/share/fonts/nerd-fonts/Iosevka $CMD_OUTPUT
+    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip &>> kalighost.out
+    unzip Iosevka.zip -d /usr/share/fonts/nerd-fonts/Iosevka &>> kalighost.out
     rm Iosevka.zip
 fi
 
@@ -115,8 +114,8 @@ if fc-list | grep -qi "RobotoMono Nerd Font"; then
     echo "RobotoMono Nerd Font already installed" | lolcat
 else
     echo "Installing RobotoMono font" | lolcat
-    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip $CMD_OUTPUT
-    unzip RobotoMono.zip -d /usr/share/fonts/nerd-fonts/RobotoMono $CMD_OUTPUT
+    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip &>> kalighost.out
+    unzip RobotoMono.zip -d /usr/share/fonts/nerd-fonts/RobotoMono &>> kalighost.out
     rm RobotoMono.zip
 fi
 
@@ -125,8 +124,8 @@ if fc-list | grep -qi "Hack"; then
     echo "Hack Nerd Font already installed" | lolcat
 else
     echo "Installing Hack Nerd font" | lolcat
-    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip $CMD_OUTPUT
-    unzip Hack.zip -d /usr/share/fonts/nerd-fonts/Hack $CMD_OUTPUT
+    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip &>> kalighost.out
+    unzip Hack.zip -d /usr/share/fonts/nerd-fonts/Hack &>> kalighost.out
     rm Hack.zip
 fi
 sudo fc-cache -f
@@ -151,17 +150,17 @@ sudo chmod +x ~/.config/rofi/power/powermenu.sh
 
 # Configure Betterlockscreen 
 echo "Configuring betterlockscreen..." | lolcat
-sudo betterlockscreen -u ~/.images/0.jpeg $CMD_OUTPUT
+sudo betterlockscreen -u ~/.images/0.jpeg &>> kalighost.out
 
 # Install tpm and Configure tmux
 if [ -d "~/.tmux/plugins/tpm" ]; then
    echo "tpm already installed." | lolcat
 else
    echo "Installing tpm..." | lolcat
-   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm $CMD_OUTPUT
+   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &>> kalighost.out
    echo "Installing Tmux plugins..." | lolcat 
    if [ -f "~/.tmux/plugins/tpm/scripts/install_plugins.sh" ]; then
-       bash " ~/.tmux/plugins/tpm/scripts/install_plugins.sh" $CMD_OUTPUT
+       bash " ~/.tmux/plugins/tpm/scripts/install_plugins.sh" &>> kalighost.out
    fi
 fi
 
@@ -170,18 +169,18 @@ if [ -d "/opt/nvim" ]; then
    echo "Neovim Already installed" | lolcat
 else
    echo "Installing Neovim" | lolcat
-   wget -q https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz -O nvim-linux64.tar.gz $CMD_OUTPUT
+   wget -q https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz -O nvim-linux64.tar.gz &>> kalighost.out
    sudo tar -C /opt -xzf nvim-linux64.tar.gz
    sudo mv /opt/nvim-linux64 /opt/nvim
    rm nvim-linux64.tar.gz
    echo "Installing NvChad after installation completes. Run 'nvim' to download plugins then ':MasonInstallAll'." | lolcat
-   git clone https://github.com/NvChad/starter ~/.config/nvim $CMD_OUTPUT
+   git clone https://github.com/NvChad/starter ~/.config/nvim &>> kalighost.out
 fi
 
 # Install Oh My Zsh
 echo "Installing Oh My Zsh..." | lolcat
 cp zshrc ~/.zshrc
-sh -c "$(curl -fsSL https://install.ohmyz.sh)" "" --unattended $CMD_OUTPUT
+sh -c "$(curl -fsSL https://install.ohmyz.sh)" "" --unattended &>> kalighost.out
 chsh -s $(which zsh)
 
 # Remove Packages
@@ -197,7 +196,7 @@ if [[ "$upackage" == "n" || "$upackage" == "N" ]]; then
     echo -e "\nPlease update your packages after a reboot using the command 'sudo apt update && sudo apt upgrade'" | lolcat
 else
     echo "Updating Packages" | lolcat
-    sudo apt update  -y $CMD_OUTPUT
+    sudo apt update  -y &>> kalighost.out
     echo -e "\nUpgrading Packages this may take sometime to complete\n" | lolcat
     sudo apt upgrade -y 
     clear
